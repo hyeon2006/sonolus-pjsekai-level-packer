1b5f06c9748bed17f87c3916d4ef4dba21aa9a5c
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2026-02-06 03:37:05 +0900

    fix: type error

--- src/packer/tag.ts
+++ src/packer/tag.ts
@@ -2,6 +2,6 @@ import type { DatabaseTag, Tag } from '@sonolus/core'
 import { localize } from './localize'
 
 export const toTag = (databaseTag: DatabaseTag): Tag => ({
-    title: localize(databaseTag.title),
+    title: databaseTag.title ? localize(databaseTag.title) : '',
     icon: databaseTag.icon,
 })
